188bcfa4f81617d338f4e14cc1dea5e60a4c6176
hengli <hengli_mail@126.com>
2016-02-22 11:50:36 +0800

    test sample

--- test.py
+++ test.py
@@ -4,12 +4,33 @@ import time
 import os
 import logging
 from tornado.ioloop import IOLoop
+import resource
+from multiprocessing import Process
+
+resource.setrlimit(resource.RLIMIT_NOFILE, (65536, 65536))
+
+ts = 0
+cnt = 0
+
+process_num = 4
+
+muti_flg = False
+if muti_flg:
+    loop_num = 2500
+else:
+    loop_num = 10000
 
 
 def cb_sync(client, data):
-    # TODO OK
-    # client.close()
     # print data, 'cb_sync'
+    global cnt
+    global ts
+
+    cnt += 1
+    # print cnt, os.getpid(), 'callback'
+    if cnt == loop_num:
+        te = time.time()
+        print te - ts
     pass
 
 
@@ -21,7 +42,7 @@ def cb_async(data):
 def test_sync_call():
     client = Client()
     client.connect('127.0.0.1', 8000)
-    client.call('sum', [1, 2], cb=lambda data: cb_sync(client, data))
+    client.call('sum', [0, 1], cb=lambda data: cb_sync(client, data))
 
 
 def test_sync_notify():
@@ -34,6 +55,7 @@ def test_async_call():
     client = Client()
     client.set_host('127.0.0.1', 8000)
     client.call_async('sum', [5, 6], cb=cb_async)
+    # client.call_async('sum', [5, 6], cb=cb_async)
 
 
 def test_async_notify():
@@ -41,6 +63,43 @@ def test_async_notify():
     client.set_host('127.0.0.1', 8000)
     client.call_async('sum', [5, 6], cb=cb_async)
 
+
+def multi_test_sync_call():
+    print "start", time.time() - ts
+    for i in xrange(loop_num):
+        # print i, os.getpid()
+        test_sync_call()
+    IOLoop.current().start()
+
+
+def muti_process_test():
+    global ts
+    ts = time.time()
+
+    for j in xrange(process_num):
+        p = Process(target=multi_test_sync_call)
+        p.start()
+
+
+def singal_process_test():
+    global ts
+    ts = time.time()
+
+    for i in xrange(loop_num):
+        # 2process 7.21562886238 10000
+        test_sync_call()
+
+        # 2process 7.70088887215 10000
+        # test_sync_notify()
+
+        # 2process 0.0398619174957 100
+        # test_async_call()
+
+        # test_async_notify()
+        # te = time.time()
+        # all_time += te - ts
+        # print te - ts
+    IOLoop.current().start()
 
 if __name__ == '__main__':
     if not logging.getLogger().handlers:
@@ -55,19 +114,8 @@ if __name__ == '__main__':
     # server.bind()
     # server.start()
 
-    ts = time.time()
-    for i in xrange(10000):
-        # 2process 7.21562886238 10000
-        # test_sync_call()
+    if muti_flg:
+        muti_process_test()
+    else:
+        singal_process_test()
 
-        # 2process 7.70088887215 10000
-        # test_sync_notify()
-
-        # 2process 0.0398619174957 100
-        test_async_call()
-
-        # test_async_notify()
-
-    print time.time() - ts
-
-    IOLoop.current().start()
